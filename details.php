ac78a347ccac109cb1d2ee3361987b9699ae1442
Jonathan <jonathanprada@protonmail.ch>
2017-03-07 23:26:57 +0100

    Add files via upload

--- details.php
+++ details.php
@@ -4,7 +4,7 @@
 include("inc/data.php");
 include("inc/functions.php");
 
-//If there is a value in the url /details.php?id=101
+/*//If there is a value in the url /details.php?id=101
 if (isset($_GET["id"])) {
     //assign that id to a variable
     $id = $_GET["id"];
@@ -23,6 +23,27 @@ if(!isset($item)){
     //make sure nothing else is processed when redirecting
     exit;
 }
+*/
+
+//If there is a value in the url /details.php?id=101
+if (isset($_GET["id"])) {
+    //assign that id to a variable
+    $id = $_GET["id"];
+    //if that id exists inside our catalog
+    if (isset($catalog[$id])) {
+      //then assign that item to a variable
+      $item = $catalog[$id];
+    }
+}
+
+//if item is not set
+if (!isset($item)) {
+  //we redirect using header function
+  //tell the browser to look for a different page
+  header("location:catalog.php");
+  exit;
+}
+
 
 //use the item title
 $pageTitle = $item["title"];
@@ -36,6 +57,13 @@ include("inc/header.php");
 
     <div class="wrapper">
 
+        <div class="breadcrumbs">
+            <a href="catalog.php">Full Catalog</a> &gt;
+            <a href="catalog.php?cat=<?php echo strtolower($item["category"]); ?>"
+            ><?php echo $item["category"]; ?> </a> &gt;
+            <?php echo $item["title"]; ?>
+        </div>
+
         <div class="media-picture">
 
         <span>
@@ -43,5 +71,72 @@ include("inc/header.php");
         </span>
 
         </div>
-    </div>
+
+<div class ="media-details">
+    <H1><?php echo $item['title']; ?></H1>
+
+   <table>
+    <tr>
+      <th>Category</th>
+      <td><?php echo $item["category"]; ?></td>
+    </tr>
+    <tr>
+      <th>Genre</th>
+      <td><?php echo $item["genre"]; ?></td>
+    </tr>
+    <tr>
+      <th>Format</th>
+      <td><?php echo $item["format"]; ?></td>
+    </tr>
+    <tr>
+      <th>Year</th>
+      <td><?php echo $item["year"]; ?></td>
+    </tr>
+    <?php
+    //display base on category
+    if (strtolower($item["category"]) == "books"){
+
+    ?>
+    <tr>
+      <th>Authors</th>
+      <!--Use implode to join multiple authors by comma -->
+      <td><?php echo implode(" ,",$item["authors"]); ?></td>
+    </tr>
+    <tr>
+      <th>Publisher</th>
+      <td><?php echo $item["publisher"]; ?></td>
+    </tr>
+    <tr>
+      <th>ISBN</th>
+      <td><?php echo $item["isbn"]; ?></td>
+    </tr>
+      <?php } else if (strtolower($item["category"]) == "movies"){?>
+
+
+    <tr>
+      <th>Director</th>
+      <td><?php echo $item["director"]; ?></td>
+    </tr>
+    <tr>
+      <th>Writers</th>
+      <td><?php echo implode(" ,",$item["writers"]); ?></td>
+    </tr>
+    <tr>
+      <th>Stars</th>
+      <td><?php echo implode(" ,",$item["stars"]); ?></td>
+    </tr>
+      <?php }
+
+     else if (strtolower($item["category"]) == "music"){
+
+    ?>
+    <tr>
+      <th>Artist</th>
+      <td><?php echo $item["artist"]; ?></td>
+    </tr>
+    <?php } ?>
+  </table>
+
+  </div>
+  </div>
 </div>
--- inc/functions.php
+++ inc/functions.php
@@ -1,13 +1,14 @@
 <?php
 
 //gets passed parameters are iterated in a for loop at index.php
-function get_item_html($id, $item) {
-  $output = "<li><a href='#'><img src='"
-          . $item["img"] ."' alt='"
-          . $item["title"] ."'>"
-          . "<p>View Details</p>"
-          ."</a></li>";
-  return $output;
+function get_item_html($id,$item) {
+    $output = "<li><a href='details.php?id="
+        . $id . "'><img src='"
+        . $item["img"] . "' alt='"
+        . $item["title"] . "' />"
+        . "<p>View Details</p>"
+        . "</a></li>";
+    return $output;
 }
 
 //defines the array and category from array
